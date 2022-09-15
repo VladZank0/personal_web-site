3c7c9d6d460dc9a395baca3e744e3b2f01594aaf
VladZank0 <101754258+VladZank0@users.noreply.github.com>
2022-09-15 21:09:46 +0300

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,9 +1,16 @@
 const scrollBtn=document.querySelector(".additionalWrapper-forBtn");
 const body=document.querySelector("body");
 
+const fixed_nav=document.querySelector(".navigation");
+
 scrollBtn.onclick=()=>{
-    window.scrollTo({
-        top: 700,
-        behavior: 'smooth'})
+    // window.scrollTo({
+    //     top: 100,
+    //     behavior: 'smooth'})
+    fixed_nav.scrollIntoView({
+        behavior:"smooth",
+       
+    });
+
 }
-    
+  
